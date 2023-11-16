cda1320e4ddb6a5749541e8470ca3ef9fa1624b3
Michael Reyna <MReyna12@live.com>
2023-11-16 10:20:15 -0600

    Add nvmrc and update Vike boilerplate

--- resourcematch-app/renderer/_default.page.server.tsx
+++ resourcematch-app/renderer/_default.page.server.tsx
@@ -5,7 +5,6 @@ export const passToClient = ['pageProps', 'urlPathname']
 import ReactDOMServer from 'react-dom/server'
 import { PageShell } from './PageShell'
 import { escapeInject, dangerouslySkipEscape } from 'vike/server'
-import logoUrl from './logo.svg'
 import type { PageContextServer } from './types'
 
 async function render(pageContext: PageContextServer) {
@@ -20,14 +19,13 @@ async function render(pageContext: PageContextServer) {
 
   // See https://vike.dev/head
   const { documentProps } = pageContext.exports
-  const title = (documentProps && documentProps.title) || 'Vite SSR app'
-  const desc = (documentProps && documentProps.description) || 'App using Vite + Vike'
+  const title = (documentProps && documentProps.title) || 'Resourcematch App'
+  const desc = (documentProps && documentProps.description) || 'Provide up to 500 characters and submit to potentially receive relevant Openstax book links'
 
   const documentHtml = escapeInject`<!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
