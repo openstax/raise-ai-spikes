c1a9466451d639647d09e81c04de7529747d9d00
Michael Reyna <MReyna12@live.com>
2023-11-17 12:13:20 -0600

    Add search query helper function

--- resourcematch-api/main.py
+++ resourcematch-api/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import AnyHttpUrl, BaseModel
 from typing import List, Literal
+import aiohttp
+import asyncio
 
 app = FastAPI(
     title="Resource Match API"
@@ -27,6 +29,57 @@ class MatchRequest(BaseModel):
 
 class MatchResponse(BaseModel):
     urls: List[AnyHttpUrl]
+
+
+async def process_search_queries(search_queries, books):
+    books_by_slug = {
+        "college-algebra-corequisite-support-2e": {
+            "uuid": "32bee6a",
+            "code_version": "20230828.164620",
+            "book_version": "59024a63-2b1a-4631-94c5-ae275a77b587"
+        },
+        "world-history-volume-2": {
+            "uuid": "685e3163-1032-4529-bb3a-f97a54412704",
+            "code_version": "20230828.164620",
+            "book_version": "244d248"
+        },
+        "us-history": {
+            "uuid": "a7ba2fb8-8925-4987-b182-5f4429d48daa",
+            "code_version": "20230828.164620",
+            "book_version": "59e152a"
+        },
+
+        "biology-2e": {
+            "uuid": "8d50a0af-948b-4204-a71d-4826cba765b8",
+            "code_version": "20230828.164620",
+            "book_version": "3bf8607"
+        }
+    }
+
+    async with aiohttp.ClientSession() as session:
+        first_book_slug = books[0]
+        book_data = books_by_slug[first_book_slug]
+        previously_used_queries = {}
+        aggregate_search_results = []
+
+        for query in search_queries:
+            if query in previously_used_queries:
+                continue
+            previously_used_queries[query] = True
+            url = (f'https://openstax.org/open-search/'
+                   f'api/v0/search?q={query}'
+                   f'&books={book_data['code_version']}/{book_data['uuid']}'
+                   f'@{book_data['book_version']}'
+                   f'&index_strategy=i1&search_strategy=s1')
+            async with session.get(url) as resp:
+                search_response = await resp.json()
+                search_info = {
+                    "book": first_book_slug,
+                    "search_query": query,
+                    "search_response": search_response
+                }
+                aggregate_search_results.append(search_info)
+        return aggregate_search_results
 
 
 @app.post("/match")
