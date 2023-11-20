0658af2931a569054aec82fcf2198f59cd53abac
Prabhdip Gill <pg53@rice.edu>
2023-11-20 10:49:30 -0800

    Invoke openai and implement generate_search_queries method

--- resourcematch-api/main.py
+++ resourcematch-api/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import AnyHttpUrl, BaseModel
 from typing import List, Literal
+import json
+from openai import OpenAI
+client = OpenAI()
 
 app = FastAPI(
     title="Resource Match API"
@@ -27,6 +30,32 @@ class MatchRequest(BaseModel):
 
 class MatchResponse(BaseModel):
     urls: List[AnyHttpUrl]
+
+
+async def generate_search_queries(openai_client, model, text):
+    content = (
+        "Given an input from a user, please provide a set of no more than "
+        "3 search phrases that would be appropriate to retrieve related "
+        "content from a search engine. Each phrase should be 3 words or less. "
+        "Return the response as a JSON string where the object has a key "
+        "'search_queries' with an array of selected terms."
+    )
+
+    chat_completion = openai_client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": content,
+            },
+            {
+                "role": "user",
+                "content": text
+            }
+        ],
+        model=model,
+    )
+    result = json.loads(chat_completion.choices[0].message.content)
+    return result['search_queries']
 
 
 @app.post("/match")
